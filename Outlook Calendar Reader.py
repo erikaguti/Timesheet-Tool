b1e53716635bb5695874cd2de2fc3ce8749e97a9
erikaguti <erikaguti27@gmail.com>
2024-11-23 14:05:47 -0500

    added date and cmd line arguments

--- Outlook Calendar Reader.py
+++ Outlook Calendar Reader.py
@@ -2,31 +2,42 @@ import pandas as pd
 from datetime import datetime
 from datetime import date
 import os
+import argparse
 
-modified = []
-for file in os.listdir(os.getcwd()):
-    if file.endswith('CSV'):
-        modified.append(os.stat(file).st_mtime)
-for file in os.listdir(os.getcwd()):    
-    if os.stat(file).st_mtime == max(modified):
-        raw = pd.read_csv(file, encoding = 'latin1')
 
-time = raw[['Subject', 'Start Date','Start Time', 'End Time', 'Description']]
+def process_data(calendar_data):
+    time = calendar_data[['Subject', 'Start Date','Start Time', 'End Time', 'Description']]
 
-weekDays = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
-days = []
-for index, row in time['Start Date'].iteritems():
-    days.append(weekDays[datetime.strptime(time['Start Date'][index], "%m/%d/%Y").weekday()])
-time['Weekday'] = days
+    weekDays = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+    days = []
+    for index, row in time['Start Date'].items():
+        days.append(weekDays[datetime.strptime(time['Start Date'][index], "%m/%d/%Y").weekday()])
+    time['Weekday'] = days
 
-Hours = []
-for index, row in time.iterrows():
-    delta = datetime.strptime(time['End Time'][index], "%I:%M:%S %p") - datetime.strptime(time['Start Time'][index], "%I:%M:%S %p")
-    Hours.append(delta.seconds/3600)
-time['Hours Worked'] = Hours
+    Hours = []
+    for index, row in time.iterrows():
+        delta = datetime.strptime(time['End Time'][index], "%I:%M:%S %p") - datetime.strptime(time['Start Time'][index], "%I:%M:%S %p")
+        Hours.append(delta.seconds/3600)
+    time['Hours Worked'] = Hours
 
-clean = time[['Weekday','Subject','Hours Worked', 'Description']]
-clean.Description = clean.Description.astype(str)
-final = clean.groupby(['Weekday','Subject']).sum()
-final['Notes'] = clean.groupby(['Weekday','Subject'])['Description'].apply(';'.join)
-final.to_excel('done.xlsx')
+    clean = time[['Start Date', 'Weekday','Subject','Hours Worked', 'Description']]
+    clean.Description = clean.Description.astype(str)
+    final = clean.groupby(['Start Date','Weekday','Subject']).sum()
+    final['Notes'] = clean.groupby(['Start Date', 'Weekday','Subject'])['Description'].apply(';'.join)
+    final.drop('Description', axis=1, inplace = True)
+    return final
+
+
+def main(input_file, output_file='output.xlsx'):
+    calendar_data = pd.read_csv(input_file)
+    df = process_data(calendar_data)
+    df.to_excel(output_file)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Convert multiband images to RGB format using rasterio.')
+    parser.add_argument('--input_file', type=str, required=True, help='Path to raw calendar data')
+    parser.add_argument('--output_file', type=str, required=False, help='Name you want to give final file')
+
+    args = parser.parse_args()
+    main(args.input_file, args.output_file)
